0108bbd49afde135caa6a16e429cddb6b9cfc413
Shogher01 <shogher.galstyan@mail.ru>
2025-02-23 21:12:07 +0400

    Added database models and data insertion script

---
+++ CapstonePMTool/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    dateOfBirth: { type: Date },
+    profileImage: { type: String },
+    boards: [{ type: Schema.Types.ObjectId, ref: 'Board' }],
+    settings: {
+        theme: { type: String, default: 'light' },  // or 'dark'
+        notifications: { type: Boolean, default: true }
+    },
+    calendarIntegration: {
+        googleCalendarId: { type: String }
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
