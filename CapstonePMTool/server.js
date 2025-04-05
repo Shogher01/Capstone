9b8c4998a7e7d4bff581d3c5db161b6072928721
Shogher01 <shogher.galstyan@mail.ru>
2025-04-06 00:11:59 +0400

    Added sign up and sign in functionalities.

--- CapstonePMTool/server.js
+++ CapstonePMTool/server.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Connect to MongoDB
-mongoose
-    .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.error("MongoDB connection error:", err));
-
-app.get("/", (req, res) => {
-    res.send("Hello, WebStorm & Node.js!");
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+//
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+//
+// // Connect to MongoDB
+// mongoose
+//     .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log("MongoDB connected"))
+//     .catch((err) => console.error("MongoDB connection error:", err));
+//
+// app.get("/", (req, res) => {
+//     res.send("Hello, WebStorm & Node.js!");
+// });
+//
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ CapstonePMTool/src/config.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const connect = mongoose.connect("mongodb://localhost:27017/Capstone");
+
+connect.then(() => {
+    console.log("Database connected!");
+})
+.catch((error) => {
+    console.error(error);
+})
+
+const LoginSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+const collection = new mongoose.model("Users", LoginSchema);
+
+module.exports = collection;
