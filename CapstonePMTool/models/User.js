f5e0f7be2755521e743517c63eea93f5bfd83f12
Shogher01 <shogher.galstyan@mail.ru>
2025-04-12 16:22:03 +0400

    Added create board and create ticket backend functionalities.

--- CapstonePMTool/models/User.js
+++ CapstonePMTool/models/User.js
@@ -5,12 +5,12 @@ const userSchema = new Schema({
     name: { type: String, required: true },
     surname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true },
+    password: { type: String, required: true },
     dateOfBirth: { type: Date },
     profileImage: { type: String },
     boards: [{ type: Schema.Types.ObjectId, ref: 'Board' }],
     settings: {
-        theme: { type: String, default: 'light' },  // or 'dark'
+        theme: { type: String, default: 'light' },
         notifications: { type: Boolean, default: true }
     },
     calendarIntegration: {
--- CapstonePMTool/src/config.js
+++ CapstonePMTool/src/config.js
@@ -19,6 +19,6 @@ const LoginSchema = new mongoose.Schema({
     }
 })
 
-const collection = new mongoose.model("Users", LoginSchema);
+const collection = new mongoose.model("User", LoginSchema);
 
 module.exports = collection;
